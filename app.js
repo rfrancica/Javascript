ba4c12e480716e1f1bc42b088c4d1300877e99b6
Roberto Francica <roberto.francica@gmail.com>
2021-10-05 20:14:15 +0200

    Collecting user input

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+function startPipeline(user_form) {
     // Create Dino Constructor
 
 
@@ -13,20 +13,31 @@
     // Create Dino Compare Method 1
     // NOTE: Weight in JSON file is in lbs, height in inches. 
 
-    
+
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+
+    // Add tiles to DOM
 
     // Remove form from screen
-
+}
 
 // On button click, prepare and display infographic
+window.document.getElementById('btn').addEventListener("click", function() {
+    var selectForm = document.forms['dino-compare'];
+    let temporary_object = {
+        name: selectForm['name'].value,
+        feet: selectForm['feet'].value,
+        inches: selectForm['inches'].value,
+        weight: selectForm['weight'].value,
+        diet: selectForm['diet'].value
+    };
+    startPipeline(temporary_object)
+});
